291be569cbbe4a90b0e14b3ba9bcbdb880fca028
alisson guedes <alissonguedes@gmail.com>
2024-04-23 09:14:05 -0300

    First commit

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -1,23 +1,23 @@
 @extends('layout.body')
 
 @section('styles')
-    @parent
-    @include('main.defaults.__styles')
+	@parent
+	@include('main.defaults.__styles')
 @endsection
 
 @section('scripts')
-    @parent
-    @include('main.defaults.__scripts')
+	@parent
+	@include('main.defaults.__scripts')
 @endsection
 
 @section('content')
-    @include('main.defaults.loading')
+	@include('main.defaults.loading')
 
-    <!-- #corpo -->
-    <div class="corpo" id="corpo">
-        @include('main.defaults.topo')
-        @parent
-        @include('main.defaults.footer')
-    </div>
-    <!-- END #corpo -->
+	<!-- #corpo -->
+	<div id="corpo" class="corpo">
+		@include('main.defaults.topo')
+		@parent
+		@include('main.defaults.footer')
+	</div>
+	<!-- END #corpo -->
 @endsection
