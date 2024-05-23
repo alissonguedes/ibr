d4bb37a52ae1c85a85db47a1ddb4674303410882
alisson guedes <alissonguedes@gmail.com>
2024-05-23 04:23:20 -0300

    Atualizações do sistema

--- resources/views/admin/home/pastores/index.blade.php
+++ resources/views/admin/home/pastores/index.blade.php
@@ -8,18 +8,20 @@
 		@if (isset($pastores) && $pastores->count() > 0)
 			<div class="row">
 				@foreach ($pastores as $pastor)
-					<div class="col s6 m3 l3">
-						<div class="avatar card transparent z-depth-0 no-padding no-margin">
+					<div class="col s6 m3 l3 mb-3">
+						<div class="avatar card transparent z-depth-0 no-padding">
 
 							@if (!$pastor->status)
 								<i class="inactive material-symbols-outlined"> visibility_off </i>
 							@endif
 
-							<div class="card-image no-padding no-margin">
-								<img src="{{ route('home.pastores.show-image', $pastor->id) . '?action=preview' }}" class="circle">
-								<div class="btn-group">
-									<x-button class="btn activator btn-floating delete material-symbols-outlined font-weight-400">delete</x-button>
-									<x-button class="icon-background btn btn-floating edit material-symbols-outlined font-weight-400" :data-href="route('admin.home.pastores.edit', $pastor->id)"> edit </x-button>
+							<div class="card-image">
+								<div class="circle grey-border border-lighten-4 border-10">
+									<img src="{{ route('home.pastores.show-image', $pastor->id) . '?action=preview' }}">
+									<div class="btn-group">
+										<x-button class="btn activator btn-floating delete material-symbols-outlined font-weight-400">delete</x-button>
+										<x-button class="icon-background btn btn-floating edit material-symbols-outlined font-weight-400" :data-href="route('admin.home.pastores.edit', $pastor->id)"> edit </x-button>
+									</div>
 								</div>
 							</div>
 
