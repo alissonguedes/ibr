40b19bc6558788d4c578d8080a63964d90f7a60e
Alisson Guedes <alissonguedes87@gmail.com>
2024-06-06 19:26:13 -0300

    Atualizações

--- resources/views/main/home/apresentacao.blade.php
+++ resources/views/main/home/apresentacao.blade.php
@@ -4,7 +4,7 @@
 			<img src="{{ route('home.apresentacao.show-image', $post->id) }}" class="img_cem" style="background-color: transparent; border-radius: 100%;">
 		</div>
 		<div class="text_ap">
-			<div class="title_ap">{{ $post->titulo }}</div>
+			<div class="title_ap bold">{{ $post->titulo }}</div>
 			<div class="sub_ap">
 				{{ $post->subtitulo }}
 			</div>
