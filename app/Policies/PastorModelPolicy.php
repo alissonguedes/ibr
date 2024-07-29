8f6450cc95c2117647ccc8b0104db048a1b38958
Alisson Guedes <alissonguedes87@gmail.com>
2024-07-29 14:54:18 -0300

    Atualizações do site

--- app/Policies/PastorModelPolicy.php
+++ app/Policies/PastorModelPolicy.php
@@ -20,7 +20,7 @@ class PastorModelPolicy
 	/**
 	 * Determine whether the user can view the model.
 	 */
-	public function view(User $user, PastorModel $pastorModel): bool
+	public function view(User $user): bool
 	{
 		return $user->nivel === 'root' || $user->nivel === 'admin' || $user->nivel === 'pastor';
 	}
@@ -36,7 +36,7 @@ class PastorModelPolicy
 	/**
 	 * Determine whether the user can update the model.
 	 */
-	public function update(User $user, PastorModel $pastorModel): bool
+	public function update(User $user): bool
 	{
 		return $user->nivel === 'root' || $user->nivel === 'admin' || $user->nivel === 'pastor';
 	}
@@ -44,7 +44,7 @@ class PastorModelPolicy
 	/**
 	 * Determine whether the user can delete the model.
 	 */
-	public function delete(User $user, PastorModel $pastorModel): bool
+	public function delete(User $user): bool
 	{
 		return $user->nivel === 'root' || $user->nivel === 'admin' || $user->nivel === 'pastor';
 	}
